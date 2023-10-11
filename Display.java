32906ae29e0e5fa9b453dbb2e68903bf888893ff
dat9395 <47808218+dat9395@users.noreply.github.com>
2023-10-11 11:53:40 +0700

    Reverse symbol for players

--- Display.java
+++ Display.java
@@ -1,49 +1,44 @@
 public abstract class Display {
-    /* Use default constructor */
-
     public static void printBoard(Board board) {
         Integer[][] boardArray = board.getArray();
         
         // Column numbering
         System.out.print("   "); // Triple-space
         for (int j = 0; j < board.getCols(); j++) {
-            if (j < 9) {
-                System.out.print(" " + (j+1) + " ");
+            if (j < 10) {
+                System.out.print(" " + j + " ");
             }
             else {
-                System.out.print((j+1) + " ");
+                System.out.print(j + " ");
             }
         }
         System.out.println();
         
         for (int i = 0; i < board.getRows(); i++) {
             // Row numbering
-            if (i < 9) {
+            if (i < 10) {
                 System.out.print(" ");
-                System.out.print(i+1);
+                System.out.print(i);
                 System.out.print(" ");
             }
             else {
-                System.out.print(i+1);
+                System.out.print(i);
                 System.out.print(" ");
             }
             
             // Main body
             for (int j = 0; j < board.getCols(); j++) {
-                if (boardArray[i][j] == null) {
+                if (boardArray[i][j] == -1) {
                     System.out.print("[#]");
                 }
-                
-                switch (boardArray[i][j]) {
-                    case 0:
-                        System.out.print("[ ]");
-                        break;
-                    case 1:
-                        System.out.print("[X]");
-                        break;
-                    case 2: 
-                        System.out.print("[O]");
-                        break;
+                else if (boardArray[i][j] == 0) {
+                    System.out.print("[ ]");
+                }
+                else if (boardArray[i][j] == 1) {
+                    System.out.print("[O]");
+                }
+                else if (boardArray[i][j] == 2) {
+                    System.out.print("[X]");
                 }
             }
             System.out.println();
